e8e379ff92f916736322f6e6852003af76ffe4a0
juan carlos gonzales montes <pope624@hotmail.com>
2017-06-29 13:06:23 -0500

    nuevas correcciones

--- C4W.Web/Controllers/HomeController.cs
+++ C4W.Web/Controllers/HomeController.cs
@@ -307,17 +307,22 @@ namespace C4W.Web.Controllers
                 ViewData["t3Cer"] = "All professional preparation in the development of Coaching competencies, its integration and implementation begins with the life aspect. This program teaches all the coaching skills from the perspective of Life Coaching, an aspect of great relevance in the training of an Executive and Corporate Coach.";
                 ViewData["t4Cer"] = "OUR CERTIFICATION PROGRAMS";
                 ViewData["t5Cer"] = "Offer two levels of certification, each one fulfilling the ICF requirements for that specific level.";
-                ViewData["t6Cer"] = "PROFESSIONAL LEVEL";
+                ViewData["t6Cer"] = "PROFESSIONAL";
+                ViewData["t6Cerco"] = "LEVEL";
                 ViewData["t6Cer6"] = "COACHING";
                 ViewData["t7Cer"] = "BUSINESS";
-                ViewData["t7jCer"] = "AND LIFE COACHING";
+                ViewData["t7Cere"] = "AND LIFE";
+                ViewData["t7jCer"] = "COACHING";
                 ViewData["t8Cer"] = "PROFESSIONAL LEVEL";
-                ViewData["t9Cer"] = "ASSOCIATE LEVEL";
+                ViewData["t9Cer"] = "ASSOCIATE";
+                ViewData["t9Cerl"] = "LEVEL";
                 ViewData["t9Cer9"] = "COACHING";
-                ViewData["t10Cer"] = "PROFESSIONAL LEVEL";
-                ViewData["t11Cer"] = "LIFE AND WELLNESS";
+                ViewData["t10Cer"] = "ASSOCIATE LEVEL";
+                ViewData["t11Cer"] = "LIFE";
+                ViewData["t11Cerw"] = "AND WELLNESS";
                 ViewData["t11Cer11"] = "COACHING";
-                ViewData["t12Cer"] = "LIFE AND SPIRITUAL";
+                ViewData["t12Cer"] = "LIFE";
+                ViewData["t12Cerv"] = "AND SPIRITUAL";
                 ViewData["t12kCer"] = "COACHING";
                 ViewData["t13Cer"] = "OASIS BREATHING TECHNIQUE";
                 //helper de presonas
@@ -556,17 +561,22 @@ namespace C4W.Web.Controllers
                 ViewData["t3Cer"] = "Toda preparación profesional en el desarrollo de las competencias de coaching, su integración y su aplicación, comienza por el área de vida.Este programa enseña todas las destrezas de coaching desde un punto de vista del coaching de vida, aspecto de gran pertinencia en la formación de un coach ejecutivo y corporativo.";
                 ViewData["t4Cer"] = "NUESTROS PROGRAMAS CERTIFICADOS";
                 ViewData["t5Cer"] = "Ofrecen dos niveles de certificación que cumplen a cabalidad con los requerimientos de la ICF para cada nivel de credencial.";
-                ViewData["t6Cer"] = "COACHING NIVEL";
+                ViewData["t6Cer"] = "COACHING";
+                ViewData["t6Cerco"] = "NIVEL";
                 ViewData["t6Cer6"] = "PROFESIONAL";
-                ViewData["t7Cer"] = "COACH EMPRESARIAL";
+                ViewData["t7Cer"] = "COACHING";
+                ViewData["t7Cere"] = "EMPRESARIAL";
                 ViewData["t7jCer"] = "Y DE VIDA";
                 ViewData["t8Cer"] = "NIVEL PROFESIONAL";
-                ViewData["t9Cer"] = "COACHING NIVEL";
+                ViewData["t9Cer"] = "COACHING ";
+                ViewData["t9Cerl"] = "NIVEL";
                 ViewData["t9Cer9"] = "ASOCIADO";
-                ViewData["t10Cer"] = "NIVEL PROFESIONAL";
-                ViewData["t11Cer"] = "COACHING DE VIDA";
+                ViewData["t10Cer"] = "NIVEL ASOCIADO ";
+                ViewData["t11Cer"] = "COACHING";
+                ViewData["t11Cerw"] = "DE VIDA";
                 ViewData["t11Cer11"] = "Y BIENESTAR";
-                ViewData["t12Cer"] = "COACHING DE VIDA";
+                ViewData["t12Cer"] = "COACHING";
+                ViewData["t12Cerv"] = "DE VIDA";
                 ViewData["t12kCer"] = "Y ESPIRITUAL";
                 ViewData["t13Cer"] = "TÉCNICA DE RESPIRACIÓN OASIS";
                 //helper de personas
@@ -804,7 +814,8 @@ namespace C4W.Web.Controllers
                 ViewData["contact_link"] = "CONTACT US";
                 //variables de servicios
                 ViewData["t1Ser"] = "SERVICES";
-                ViewData["t2Ser"] = "ONE-ON-ONE COACHING";
+                ViewData["t2Ser"] = "ONE-ON-ONE";
+                ViewData["t2Serc"] = "COACHING";
                 ViewData["t3Ser"] = "We offer individual services in";
                 ViewData["t4Ser"] = "Life Coaching";
                 ViewData["t5Ser"] = "Executive and Business Coaching(e.g., teams, sales)";
@@ -813,14 +824,17 @@ namespace C4W.Web.Controllers
                 ViewData["t8Ser"] = "Spiritual Coaching";
                 ViewData["t9Ser"] = "Contact us for additional information regarding our services.";
                 ViewData["t10Ser"] = "WEBINARS";
+                ViewData["t10Serl"] = "";
                 ViewData["t11Ser"] = "We offer online seminars about diverse topics, geared towards:";
                 ViewData["t12Ser"] = "People interested in learning more about coaching and being coached";
                 ViewData["t13Ser"] = "People interested in becoming certified coaches";
                 ViewData["t14Ser"] = "Certified coaches";
                 ViewData["t15Ser"] = "General public";
                 ViewData["t16Ser"] = "Contact us for additional information regarding our webinars.";
-                ViewData["t17Ser"] = "MENTORING FOR COACHES";
+                ViewData["t17Ser"] = "MENTORING FOR";
+                ViewData["t17Serco"] = "COACHES";
                 ViewData["t18Ser"] = "Geared toward coaches seeking to improve their coaching skills, or coaches interested in complying with the mentoring requirements for ICF credentials.";
+                ViewData["t18Seru"] = "Contact us for additional information regarding our program.";
             }
             //texto en el idioma español
             else
@@ -835,7 +849,8 @@ namespace C4W.Web.Controllers
                 ViewData["contact_link"] = "CONTÁCTANOS";
                 //variables de servicios
                 ViewData["t1Ser"] = "SERVICIOS";
-                ViewData["t2Ser"] = "COACHING INDIVIDUAL";
+                ViewData["t2Ser"] = "COACHING";
+                ViewData["t2Serc"] = "INDIVIDUAL";
                 ViewData["t3Ser"] = "Ofrecemos servicios individualizados de coaching:";
                 ViewData["t4Ser"] = "Coaching de vida";
                 ViewData["t5Ser"] = "Coaching ejecutivo y de negocios (Ej., equipos, ventas)";
@@ -843,15 +858,18 @@ namespace C4W.Web.Controllers
                 ViewData["t7Ser"] = "Coaching de bienestar";
                 ViewData["t8Ser"] = "Coaching espiritual";
                 ViewData["t9Ser"] = "Contáctenos para información adicional sobre nuestros servicios.";
-                ViewData["t10Ser"] = "CHARLAS EN LÍNEA";
+                ViewData["t10Ser"] = "CHARLAS EN";
+                ViewData["t10Serl"] = "LÍNEA";
                 ViewData["t11Ser"] = "Ofrecemos charlas virtuales (webinars) sobre diversos temas para:";
                 ViewData["t12Ser"] = "Interesados en conocer más información sobre coaching y/o recibir servicios de coaching";
                 ViewData["t13Ser"] = "Interesados en formarse como coaches";
                 ViewData["t14Ser"] = "Coaches certificados";
                 ViewData["t15Ser"] = "Público en general";
                 ViewData["t16Ser"] = "Contáctenos para información adicional sobre nuestras charlas y otros eventos.";
-                ViewData["t17Ser"] = "MENTORIAS PARA COACHES";
-                ViewData["t18Ser"] = "Ofrecemos servicios individualizados y grupales de mentoría en coaching dirigidos a coaches en formación y/o certificados que deseen mejorar sus destrezas de coaching, o para coaches interesados en cumplir con el requisito de mentorías  que les permita solicitar credenciales ante la ICF. Contáctenos para información adicional sobre nuestros programa";
+                ViewData["t17Ser"] = "MENTORIAS PARA";
+                ViewData["t17Serco"] = "COACHES";
+                ViewData["t18Ser"] = "Ofrecemos servicios individualizados y grupales de mentoría en coaching dirigidos a coaches en formación y/o certificados que deseen mejorar sus destrezas de coaching, o para coaches interesados en cumplir con el requisito de mentorías  que les permita solicitar credenciales ante la ICF.";
+                ViewData["t18Seru"] = "Contáctenos para información adicional sobre nuestros programa";
             }
             
             return View();
